dd4aef43305419a19ff672cd85adad9edc952d12
Peter Hankiewicz <peter.hankiewicz@gmail.com>
2023-01-19 14:04:05 +0100

    Remove domain part from usernames

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def clean_username(username)
+    username.gsub(/\.law\.harvard\.edu|\.harvard\.edu/, '')
+  end
 end
