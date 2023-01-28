c0b5dd952faaa21ff86bf5cae07992ba49d1916d
Peter Hankiewicz <peter.hankiewicz@gmail.com>
2023-01-28 20:43:50 +0100

    Fix issue with reading CAS data files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -42,25 +42,37 @@ class ApplicationController < ActionController::Base
 
     cas_data_file = "#{ENV['CAS_DATA_DIRECTORY']}/#{cookies[:MOD_AUTH_CAS]}"
     user_data = {}
+    tries = 5
 
     begin
-      cas_data_file_content = File.read(cas_data_file)
-      cas_data_file_parsed = Nokogiri::XML('<?xml version="1.0" encoding="utf-8"?>' + cas_data_file_content)
-      cas_data_file_parsed.root.add_namespace_definition('def', 'http://uconn.edu/cas/mod_auth_cas')
+      # CAS data files are periodically refreshed and rewritten by the Apache
+      # module, we don't want to end up reading them in the middle of this
+      # operation.
+      (1..tries).to_a.each do
+        cas_data_file_content = File.read(cas_data_file)
+        cas_data_file_parsed = Nokogiri::XML('<?xml version="1.0" encoding="utf-8"?>' + cas_data_file_content)
+        cas_data_file_parsed.root.add_namespace_definition('def', 'http://uconn.edu/cas/mod_auth_cas')
 
-      cas_data_file_parsed.search('//def:attribute').each do |attribute|
-        if attribute.attributes['name'].value == 'mail'
-          attribute_value_node = attribute.children.find { |child| child.name == 'value' }
-          user_data['email'] = attribute_value_node.text
+        cas_data_file_parsed.search('//def:attribute').each do |attribute|
+          if attribute.attributes['name'].value == 'mail'
+            attribute_value_node = attribute.children.find { |child| child.name == 'value' }
+            user_data['email'] = attribute_value_node.text
+          end
+
+          if attribute.attributes['name'].value == 'displayName'
+            attribute_value_node = attribute.children.find { |child| child.name == 'value' }
+            user_data['name'] = attribute_value_node.text
+          end
         end
 
-        if attribute.attributes['name'].value == 'displayName'
-          attribute_value_node = attribute.children.find { |child| child.name == 'value' }
-          user_data['name'] = attribute_value_node.text
+        if user_data['email'].present? == false || user_data['name'].present? == false
+          sleep 0.1
+        else
+          break
         end
       end
 
-      raise("Email or name missing for #{cas_data_file}") if (user_data['email'].present? == false) || (user_data['name'].present? == false)
+      raise("Email or name missing for #{cas_data_file}") if user_data['email'].present? == false || user_data['name'].present? == false
 
       user = User.where(email: user_data['email']).first
       unless user
