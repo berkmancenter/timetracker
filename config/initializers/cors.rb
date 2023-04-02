b4e58224c357b7ea13b00461097e27924ac9bc93
Peter Hankiewicz <peter.hankiewicz@gmail.com>
2023-04-02 11:04:36 +0200

    Safelist CORS only in development

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,8 @@
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins 'http://127.0.0.1:6767'
-    resource '*', headers: :any, methods: :any, credentials: true
+if Rails.env.development?
+  Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins 'http://127.0.0.1:6767'
+      resource '*', headers: :any, methods: :any, credentials: true
+    end
   end
 end
