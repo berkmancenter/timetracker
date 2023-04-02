86c1d00892e2ee47aadca9260a46ab88973f67e3
Peter Hankiewicz <peter.hankiewicz@gmail.com>
2023-04-02 10:13:21 +0200

    Convert rest of models to AR style

--- app/models/period.rb
+++ app/models/period.rb
@@ -11,43 +11,19 @@ class Period < ActiveRecord::Base
     current_passed = current_num_of_days / total_num_of_days
     current_passed = 1 if current_passed > 1
 
-    query = "
-      SELECT
-        u.username,
-        u.email,
-        COALESCE(SUM(t.decimal_time), 0) as total_hours,
-        c.amount as credits
-      FROM
-        users u
-      JOIN
-        credits c
-        ON
-        u.id=c.user_id
-      LEFT JOIN
-        time_entries t
-        ON
-        u.id=t.user_id
-      WHERE
-        (t.created_at >= '#{self.from.to_time}'
-        AND
-        t.created_at <= '#{self.to.to_time.end_of_day}')
-        OR
-        t.created_at IS NULL
-      GROUP BY
-        u.username,
-        u.email,
-        c.amount
-      ORDER BY
-        u.username
-    "
-
-    records_array = ActiveRecord::Base.connection.execute(query)
-
-    records_array.map do |record|
-      record['should_hours'] = current_passed * record['credits']
-      record['balance'] = record['total_hours'] - record['should_hours']
-      record['balance_percent'] = (record['total_hours'] / record['should_hours']) * 100
-      record
-    end
+    User
+      .select('users.username, users.email, COALESCE(SUM(time_entries.decimal_time), 0) AS total_hours, credits.amount AS credits')
+      .joins("LEFT JOIN time_entries ON time_entries.user_id = users.id AND (time_entries.created_at >= '#{self.from.to_time}' AND time_entries.created_at <= '#{self.to.to_time.end_of_day}')")
+      .joins(:credits)
+      .where('credits.period_id = ?', self.id)
+      .group('users.username, users.email, credits.amount')
+      .order('users.username')
+      .map do |record|
+        record = record.attributes
+        record['should_hours'] = current_passed * record['credits'].to_f
+        record['balance'] = record['total_hours'].to_f - record['should_hours']
+        record['balance_percent'] = (record['total_hours'].to_f / record['should_hours']) * 100
+        record
+      end
   end
 end
