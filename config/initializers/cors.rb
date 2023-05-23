30e0d417744773f44627b2ff4132d036d2f81b4a
Peter Hankiewicz <peter.hankiewicz@gmail.com>
2023-05-23 12:54:49 +0200

    Allow to set allowed CORS origins in env

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 if Rails.env.development?
   Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://127.0.0.1:6767'
+      origins (ENV['CORS_ALLOWED_ORIGINS'] || []).split(',')
       resource '*', headers: :any, methods: :any, credentials: true
     end
   end
