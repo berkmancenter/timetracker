2c624379d2c7c56c29492aa950bc62f74ee4e2c9
Peter Hankiewicz <peter.hankiewicz@gmail.com>
2023-09-03 21:06:44 +0200

    Make initial devise migration revertable

--- db/migrate/20230831181131_add_devise_to_users.rb
+++ db/migrate/20230831181131_add_devise_to_users.rb
@@ -1,18 +1,33 @@
 class AddDeviseToUsers < ActiveRecord::Migration[7.0]
-  def self.up
+  def change
     change_table :users do |t|
+      ## Database authenticatable
       t.string :encrypted_password, null: false, default: ''
 
+      ## Recoverable
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+
+      ## Rememberable
       t.datetime :remember_created_at
+
+      ## Trackable
       t.integer  :sign_in_count, default: 0, null: false
       t.datetime :current_sign_in_at
       t.datetime :last_sign_in_at
       t.string   :current_sign_in_ip
       t.string   :last_sign_in_ip
-    end
-  end
 
-  def self.down
-    raise ActiveRecord::IrreversibleMigration
+      ## Confirmable
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+      t.string   :unconfirmed_email
+
+      ## Lockable
+      t.integer  :failed_attempts, default: 0, null: false
+      t.string   :unlock_token
+      t.datetime :locked_at
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -90,12 +90,21 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_02_191510) do
     t.datetime "updated_at", precision: nil
     t.string "email", limit: 255
     t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
     t.string "password", default: "", null: false
     t.index ["superadmin"], name: "index_users_on_superadmin"
     t.index ["username"], name: "index_users_on_username", unique: true
