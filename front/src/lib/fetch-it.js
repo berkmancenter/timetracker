df73d6ea066251b5af55cabc99e3115484124ec1
Peter Hankiewicz <peter.hankiewicz@gmail.com>
2023-09-21 19:27:44 +0200

    Don't catch API connection errors

--- front/src/lib/fetch-it.js
+++ front/src/lib/fetch-it.js
@@ -3,12 +3,7 @@ import { globals } from '@/main.js'
 const fetchIt = async (url, options = {}) => {
   options.credentials = 'include'
 
-  let response
-  try {
-    response = await fetch(url, options)
-  } catch (error) {
-    globals.awn.warning('Something went wrong.<br>Please refresh the page or try again later.')
-  }
+  const response = await fetch(url, options)
 
   const responsePromise = new Promise((resolve, reject) => {
     if (response) {
