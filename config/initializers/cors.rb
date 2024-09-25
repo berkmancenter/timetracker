3c9cd48db510ae943d3b48c202b320753bd6485c
Peter Hankiewicz <peter.hankiewicz@gmail.com>
2024-09-25 16:22:53 +0200

    Remove unused code from cors initializer

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,6 @@
-#if Rails.env.development?
-  Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins (ENV['CORS_ALLOWED_ORIGINS'] || []).split(',')
-      resource '*', headers: :any, methods: :any, credentials: true
-    end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins (ENV['CORS_ALLOWED_ORIGINS'] || []).split(',')
+    resource '*', headers: :any, methods: :any, credentials: true
   end
-#end
+end
